a98b031d9796a65498a7351dd3348f17d25c4264
Squeezy <percevalfeng123@gmail.com>
2018-04-24 15:45:21 -0700

    minor style fix

--- bookclub.js
+++ bookclub.js
@@ -1,5 +1,5 @@
 
-/* Called when the user pushes the "submit" button */
+/* Called when the user pushes the 'submit' button */
 /* Sends a request to the API using the JSONp protocol */
 
 var book_counter = 0;
@@ -8,43 +8,43 @@ var bookList;
 function transition() {
 	var w = parseInt(window.innerWidth)
 	if(w  > 600){
-		var text = document.getElementById("Search");
-		text.style.display = "none";
+		var text = document.getElementById('Search');
+		text.style.display = 'none';
 
-		var head = document.getElementById("book_club");
-		head.style.width = "10%";
+		var head = document.getElementById('book_club');
+		head.style.width = '10%';
 
-		var header = document.getElementById("header");
-		header.style.display = "flex";
+		var header = document.getElementById('header');
+		header.style.display = 'flex';
 
 		$(document).ready(function() {
-				$("#place_holder").appendTo("header");
-				$("#button").appendTo("header");
+				$('#place_holder').appendTo('header');
+				$('#button').appendTo('header');
 	 });
 
-	 document.getElementById("place_holder").style.marginLeft = "0px";
-	 document.getElementById("button").style.marginLeft = "0px";
-	 document.getElementById("button2").style.margin = "0px";
+	 document.getElementById('place_holder').style.marginLeft = '0px';
+	 document.getElementById('button').style.marginLeft = '0px';
+	 document.getElementById('button2').style.margin = '0px';
 	}
 	else{
-		var head = document.getElementById("book_club");
-		// head.style.width = "90%";
+		var head = document.getElementById('book_club');
+		// head.style.width = '90%';
 
-		var header = document.getElementById("header");
-		head.style.display = "flex";
+		var header = document.getElementById('header');
+		head.style.display = 'flex';
 
-		var text = document.getElementById("Search");
-		text.style.display = "none";
+		var text = document.getElementById('Search');
+		text.style.display = 'none';
 
-		var placeHolder = document.getElementById("place_holder");
-		placeHolder.style.display = "none";
+		var placeHolder = document.getElementById('place_holder');
+		placeHolder.style.display = 'none';
 
-		var button = document.getElementById("button");
-		button.style.display = "none";
+		var button = document.getElementById('button');
+		button.style.display = 'none';
 
-		var sign = document.getElementById("sign");
-		sign.style.width="10%";
-		sign.style.display = "flex";
+		var sign = document.getElementById('sign');
+		sign.style.width='10%';
+		sign.style.display = 'flex';
 	}
 
 
@@ -52,33 +52,33 @@ function transition() {
 
 function newRequest() {
 
-	var author = document.getElementById("author").value;
+	var author = document.getElementById('author').value;
 	author = author.trim();
-	author = author.replace(" ","+");
+	author = author.replace(' ','+');
 
-	var title = document.getElementById("title").value;
+	var title = document.getElementById('title').value;
 	title = title.trim();
-	title = title.replace(" ","+");
+	title = title.replace(' ','+');
 
-	var isbn = document.getElementById("isbn").value;
+	var isbn = document.getElementById('isbn').value;
 	isbn = isbn.trim();
-	isbn = isbn.replace("-","");
+	isbn = isbn.replace('-','');
 
-	var description = document.getElementById("description");
-	if(description != null){
+	var description = document.getElementById('description');
+	if(description != null) {
 		description = description.trim();
 	}
 
 	// Connects possible query parts with pluses
-	var query = ["",title,author,isbn].reduce(fancyJoin);
+	var query = ['',title,author,isbn].reduce(fancyJoin);
 
 	// The JSONp part.  Query is executed by appending a request for a new
 	// Javascript library to the DOM.  It's URL is the URL for the query.
 	// The library returned just calls the callback function we specify, with
 	// the JSON data we want as an argument.
-	if (query != "") {
+	if (query != '') {
 		// remove old script
-		var oldScript = document.getElementById("jsonpCall");
+		var oldScript = document.getElementById('jsonpCall');
 		if (oldScript != null) {
 			document.body.removeChild(oldScript);
 		}
@@ -86,11 +86,11 @@ function newRequest() {
 		var script = document.createElement('script');
 
 		// build up complicated request URL
-		var beginning = "https://www.googleapis.com/books/v1/volumes?q="
-		var callback = "&callback=handleResponse"
+		var beginning = 'https://www.googleapis.com/books/v1/volumes?q='
+		var callback = '&callback=handleResponse'
 
 		script.src = beginning+query+callback
-		script.id = "jsonpCall";
+		script.id = 'jsonpCall';
 
 		// put new script into DOM at bottom of body
 		document.body.appendChild(script);
@@ -99,14 +99,14 @@ function newRequest() {
 
 /* Used above, for joining possibly empty strings with pluses */
 function fancyJoin(a,b) {
-    if (a == "") { return b; }	    else if (b == "") { return a; }    else { return a+"+"+b; }}
+    if (a == '') { return b; }	    else if (b == '') { return a; }    else { return a+'+'+b; }}
 
 /* The callback function, which gets run when the API returns the result of our query */
 /* Replace with your code! */
 function handleResponse(bookListObj) {
 	bookList = bookListObj.items;
-	var overlay = document.getElementById("overlay");
-	overlay.style.display = "block";
+	var overlay = document.getElementById('overlay');
+	overlay.style.display = 'block';
 	replace();
 }
 
@@ -117,37 +117,37 @@ function getTile() {
 	var descriptions = book.volumeInfo.description;
 	var cover = book.volumeInfo.imageLinks.thumbnail;
 
-	var tile = document.createElement("div");
+	var tile = document.createElement('div');
 	tile.setAttribute('class', 'div1');
 	tile.id = book_counter;
 
-	var coverPgh = document.createElement("img");
+	var coverPgh = document.createElement('img');
 	coverPgh.src = cover;
 	tile.append(coverPgh);
 	//
-	var subTile = document.createElement("div");
-	subTile.setAttribute("class", "div2");
+	var subTile = document.createElement('div');
+	subTile.setAttribute('class', 'div2');
 	tile.append(subTile);
 	//
-	var titlePgh = document.createElement("p");
+	var titlePgh = document.createElement('p');
 	// 		/* ALWAYS AVOID using the innerHTML property */
 	titlePgh.textContent = title;
 	subTile.append(titlePgh);
 	//
-	var authorPgh = document.createElement("p");
+	var authorPgh = document.createElement('p');
 	authorPgh.textContent = author;
 	subTile.append(authorPgh);
 	//
 
 	if (descriptions == null) {
-		var emptyDescription = document.createElement("p");
+		var emptyDescription = document.createElement('p');
 		subTile.append(emptyDescription);
 		return tile;
 	}
 
-	var des = descriptions.split(" ", 30);
-	var first30des = des.join(" ") + ". . .";
-	var first30desPgh = document.createElement("p");
+	var des = descriptions.split(' ', 30);
+	var first30des = des.join(' ') + '. . .';
+	var first30desPgh = document.createElement('p');
 	first30desPgh.textContent = first30des;
 	subTile.append(first30desPgh);
 
@@ -155,53 +155,57 @@ function getTile() {
 }
 
 function off() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById('overlay').style.display = 'none';
 }
 
 function keep() {
-		var tileWrapper = document.getElementById("tileWrapper");
+		var tileWrapper = document.getElementById('tileWrapper');
 		var cur_tile = getTile();
 		tileWrapper.append(cur_tile);
 		// response();
 }
 
-function replace(){
-	var toBeReplaced = document.getElementById("tileWrapOverlay");
+function replace() {
+	var toBeReplaced = document.getElementById('tileWrapOverlay');
 	var replacement = getTile();
 	var copy_replacement = replacement.cloneNode(true);
 	toBeReplaced.replaceChild(copy_replacement, toBeReplaced.childNodes[3]);
 
 }
 
-function left(){
-		if(book_counter == 0){
-			document.getElementById(book_counter).style.display = "none";
-			document.getElementById("left").style.display = "none";
-		}
-		else{
-			document.getElementById("right").style.display = "block";
-			document.getElementById("left").style.display = "block";
-			// document.getElementById(book_counter).style.display = "none";
-			if(book_counter - 1 >= 0){
-				book_counter = book_counter - 1;
-				replace();
+function left() {
+	if(book_counter == 0) {
+		document.getElementById(book_counter).style.display = 'none';
+		document.getElementById('left').style.display = 'none';
+	} else {
+		document.getElementById('right').style.display = 'block';
+		document.getElementById('left').style.display = 'block';
+		// document.getElementById(book_counter).style.display = 'none';
+		if (book_counter - 1 >= 0) {
+			book_counter = book_counter - 1;
+			if (book_counter == 0) {
+				document.getElementById('left').style.display = 'none';
 			}
+			replace();
 		}
-
+	}
 }
 
-function right(){
-		if(book_counter == bookList.length - 1 ){
-			// document.getElementById(book_counter).style.display = "none";
-			document.getElementById("right").style.display = "none";
+function right() {
+		if(book_counter == bookList.length - 1 ) {
+			// document.getElementById(book_counter).style.display = 'none';
+			document.getElementById('right').style.display = 'none';
 		}
 		else{
-			document.getElementById("right").style.display = "block";
-			document.getElementById("left").style.display = "block";
-			// document.getElementById(book_counter).style.display = "none";
-			if(book_counter + 1 <= bookList.length){
+			document.getElementById('right').style.display = 'block';
+			document.getElementById('left').style.display = 'block';
+			// document.getElementById(book_counter).style.display = 'none';
+			if(book_counter + 1 <= bookList.length) {
 				book_counter = book_counter + 1;
 				replace();
+				if (book_counter == bookList.length - 1) {
+					document.getElementById('right').style.display = 'none';
+				}
 			}
 		}
 }
